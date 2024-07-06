69df44b714140862e9f87896f752c98da45635ac
eyal-dcoya <eyal@dcoya.com>
2024-07-06 20:51:41 +0300

    use of hashmaps to improve complexity

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from src.graph.edge import Edge, EdgeType
 from src.graph.graph_node import GraphNode
 
@@ -6,6 +8,8 @@ class Graph:
     def __init__(self):
         self.nodes = {}
         self.edges = []
+        self.parent_map = {}
+        self.permission_map = defaultdict(list) #nice trick ^_^ to avoid the if and assignment of []
 
     def add_node(self, node: GraphNode):
         """
@@ -22,6 +26,14 @@ class Graph:
         """
         self.edges.append(edge)
 
+        # Maintain parent_map
+        if edge.type == EdgeType.PARENT:
+            self.parent_map[edge.to_node.id] = edge.from_node  # Storing parent node object
+
+        # Maintain permission_map
+        if edge.type == EdgeType.PERMISSION:
+            self.permission_map[edge.to_node.id].append((edge.from_node.id, edge.type.name))
+
     def get_resource_hierarchy(self, resource_id: str):
         """
         Get the hierarchy of a given resource.
@@ -31,11 +43,10 @@ class Graph:
         hierarchy = []
         current_node = self.nodes.get(resource_id)
         while current_node:
-            parent_edge = next(
-                (edge for edge in self.edges if edge.to_node == current_node and edge.type == EdgeType.PARENT), None)
-            if parent_edge:
-                hierarchy.append(parent_edge.from_node.id)
-                current_node = parent_edge.from_node
+            parent_node = self.parent_map.get(current_node.id)
+            if parent_node:
+                hierarchy.append(parent_node.id)
+                current_node = parent_node
             else:
                 current_node = None
         return hierarchy
@@ -47,9 +58,11 @@ class Graph:
         :return: A list of tuples (resource_id, resource_type, role).
         """
         permissions = []
-        for edge in self.edges:
-            if edge.from_node.id == identity_id and edge.type == EdgeType.PERMISSION:
-                permissions.append((edge.to_node.id, edge.to_node.type.name, edge.type.name))
+        for resource_id, roles in self.permission_map.items():
+            for from_node_id, role in roles:
+                if from_node_id == identity_id:
+                    resource_type = self.nodes[resource_id].type.name
+                    permissions.append((resource_id, resource_type, role))
         return permissions
 
     def get_resource_identities(self, resource_id: str):
@@ -58,8 +71,4 @@ class Graph:
         :param resource_id: The ID of the resource.
         :return: A list of tuples (identity_id, role).
         """
-        identities = []
-        for edge in self.edges:
-            if edge.to_node.id == resource_id and edge.type == EdgeType.PERMISSION:
-                identities.append((edge.from_node.id, edge.type.name))
-        return identities
+        return self.permission_map.get(resource_id, [])
